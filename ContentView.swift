07a2564d2b8b9f2a9648d435107154ca61e0ba15
justin <jcarrillo7045@stu.d214.org>
2024-04-10 14:40:58 -0500

    i added a textfield

--- ContentView.swift
+++ ContentView.swift
@@ -4,7 +4,7 @@ struct ContentView: View {
     @State var number1 = 0
     @State var number2 = 0
     var body: some View {
-        
+        TextField("0", value: $number1, format: .number)
         TextField("3", value: $number2, format: .number)
         
     }
