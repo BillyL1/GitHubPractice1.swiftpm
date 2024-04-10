b29dbd27bb28e5c280738481cf659b8efa178835
Billy <wlinker4256@stu.d214.org>
2024-04-10 14:43:23 -0500

    Added a "answer" variable and a text

--- ContentView.swift
+++ ContentView.swift
@@ -3,9 +3,11 @@ import SwiftUI
 struct ContentView: View {
     @State var number1 = 0
     @State var number2 = 0
+    @State var answer = ""
     var body: some View {
+        
         TextField("0", value: $number1, format: .number)
         TextField("3", value: $number2, format: .number)
-        
+        Text(answer)
     }
 }
